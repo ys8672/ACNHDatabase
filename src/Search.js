ba7bb36490cd70c13787cb2137b5304840645ff7
shengye000 <kevinyesheng@gmail.com>
2020-08-13 16:24:02 -0500

    Halfway Done With Search

--- src/Search.js
+++ src/Search.js
@@ -8,17 +8,43 @@ class Search extends React.Component {
 		super(props);
 		this.myRef = React.createRef()
 		this.state = {
+			canShow: false,
 			searchText: '',
 			searchTextArray: [],
 			villagers: [],
-			villagersFiltered: []
+			villagersFiltered: [],
+			songs: [],
+			songsFiltered: [],
+			sea: [],
+			seaFiltered:[],
+			recipes: [],
+			recipesFiltered: [],
+			items: [],
+			itemsFiltered: []
 		}
 		this.searchData = this.searchData.bind(this)
+		this.scrollDivItems = React.createRef();
+		this.scrollDivRecipes = React.createRef();
+		this.scrollDivSea = React.createRef();
+		this.scrollDivSongs = React.createRef();
+		this.scrollDivVillagers = React.createRef();
 	}
 	
 	componentDidMount() {
 		fetch('/api/villagers/').then(r => r.json()).then(villager_data => {
             this.setState({villagers: villager_data.villagers})
+        })
+		fetch('/api/songs/').then(r => r.json()).then(song_data => {
+            this.setState({songs: song_data.songs})
+        })
+		fetch('/api/sea/').then(r => r.json()).then(sea_data => {
+            this.setState({sea: sea_data.sea})
+        })
+		fetch('/api/recipes/').then(r => r.json()).then(recipe_data => {
+            this.setState({recipes: recipe_data.recipes})
+        })
+		fetch('/api/items/').then(r => r.json()).then(item_data => {
+            this.setState({items: item_data.items})
         })
 	}
 	
@@ -41,13 +67,19 @@ class Search extends React.Component {
 	}
 	
 	searchData() {
+		this.setState({canShow: 1})
 		let input = document.getElementById("searchBar").value;
         this.myRef.current = input
 		this.setState({
             searchText: input
         });
 		if (input === ""){
+			this.setState({canShow: false})
 			this.setState({villagersFiltered: []})
+			this.setState({songsFiltered: []})
+			this.setState({seaFiltered: []})
+			this.setState({recipesFiltered: []})
+			this.setState({itemsFiltered: []})
 		}
 		else{
 			var inputSplit = input.split(" ");
@@ -57,7 +89,18 @@ class Search extends React.Component {
 			//sort villagers
 			var villagerList = this.sortFunc(inputSplit, this.state.villagers)
 			this.setState({villagersFiltered: villagerList})
-
+			//sort songs
+			var songList = this.sortFunc(inputSplit, this.state.songs)
+			this.setState({songsFiltered: songList})
+			//sea
+			var seaList = this.sortFunc(inputSplit, this.state.sea)
+			this.setState({seaFiltered: seaList})
+			//DIY recipes
+			var recipeList = this.sortFunc(inputSplit, this.state.recipes)
+			this.setState({recipesFiltered: recipeList})
+			//Items
+			var itemList = this.sortFunc(inputSplit, this.state.items)
+			this.setState({itemsFiltered: itemList})
 		}
 	}
 	
@@ -68,7 +111,7 @@ class Search extends React.Component {
 			  <title>{ TITLE }</title>
 			</Helmet>
 			<div>
-				<h1 className='text-center'><b>Search:</b></h1>
+				<h1 className='text-center'><b>Search</b></h1>
 				<br/>
 				<div style={{display: 'flex', justifyContent: 'center'}}>
                         <img className="img-center"
@@ -83,23 +126,97 @@ class Search extends React.Component {
 					<button type="submit" class="btn btn-primary btn-lg" onClick={this.searchData}>Search
 					</button>
 				</div>
-				<br/>
-				<h3 className='text-center'><b>You searched: {this.myRef.current} </b></h3>
-				<h5 className='text-center'>Number of results: {(this.state.villagersFiltered).length}</h5>
-				<br/>
+				<br/>				
 			</div>
-			
-			<div>
-			{(this.state.villagersFiltered).map((villager, i) =>
-				<div class="borderdiv" key={i}>
-					<h1 className='text-center'><b>Villager: {villager.name}</b></h1>
-					<p className="text-center"><b>Personality:</b> {villager.personality} &emsp; <b>Birthday:</b> {villager.birthday} &emsp;
-					<b>Species:</b> {villager.species} &emsp; <b>Gender:</b> {villager.gender} &emsp; <b>Catch Phrase:</b> {villager.catchPhrase}
-					&emsp; <a href={villager.image}><b>Image</b></a></p>
-					<br/>
+			<br/>
+			{this.state.canShow && <div>
+				<div style={{display: 'flex', justifyContent: 'center'}}>
+					<div class="btn-group" role="group" aria-label="Basic example">
+						<button type="button" class="btn btn-secondary" onClick={() => {
+								this.scrollDivItems.current.scrollIntoView({ behavior: 'smooth' });
+							  }}>Items</button>
+						<button type="button" class="btn btn-secondary" onClick={() => {
+								this.scrollDivRecipes.current.scrollIntoView({ behavior: 'smooth' });
+							  }}>Recipes</button>
+						<button type="button" class="btn btn-secondary" onClick={() => {
+								this.scrollDivSea.current.scrollIntoView({ behavior: 'smooth' });
+							  }}>Sea Creatures</button>
+						<button type="button" class="btn btn-secondary" onClick={() => {
+								this.scrollDivSongs.current.scrollIntoView({ behavior: 'smooth' });
+							  }}>Songs</button>
+						<button type="button" class="btn btn-secondary" onClick={() => {
+								this.scrollDivVillagers.current.scrollIntoView({ behavior: 'smooth' });
+							  }}>Villagers</button>
+					</div>
 				</div>
-			)}
-			</div>
+				<br/>
+				
+				<h1 className='text-center'><b>You searched: {this.myRef.current} </b></h1>
+				<h5 className='text-center'>Total Number of results: {(this.state.seaFiltered).length + (this.state.villagersFiltered).length
+					+ (this.state.songsFiltered).length + (this.state.recipesFiltered).length + (this.state.itemsFiltered).length}</h5>
+				<br/>
+				
+				<div ref={this.scrollDivItems}>
+					<h3 className='text-center'><b>Items: </b></h3>
+					<h5 className='text-center'>Number of items: {(this.state.itemsFiltered).length}</h5>
+						{(this.state.itemsFiltered).map((item, i) =>
+							<div class="borderdiv" key={i}>
+								<h5 className='text-center capitalize'><b>{item.name}</b></h5>
+								<p className='text-center'><a href={item.image} target="_blank"><b>Image</b></a></p>
+							</div>
+						)}  
+				</div>
+				<br/>
+				
+				<div ref={this.scrollDivRecipes}>
+					<h3 className='text-center'><b>Recipes: </b></h3>
+					<h5 className='text-center'>Number of recipes: {(this.state.recipesFiltered).length}</h5>
+						{(this.state.recipesFiltered).map((recipe, i) =>
+							<div class="borderdiv" key={i}>
+								<h5 className='text-center capitalize'><b>{recipe.name}</b></h5>
+								<p className='text-center'>Source: {recipe.source} </p>
+								<p className='text-center'>Materials: {recipe.materials} </p>
+							</div>
+						)}  
+				</div>
+				<br/>
+				
+				<div ref={this.scrollDivSea}>
+					<h3 className='text-center'><b>Sea Creatures:</b></h3>
+					<h5 className='text-center'>Number of sea creatures: {(this.state.seaFiltered).length}</h5>
+						{(this.state.seaFiltered).map((sea, i) =>
+							<div class="borderdiv" key={i}>
+								<h5 className='text-center capitalize'><b>{sea.name}</b></h5>
+								<p className='text-center'><a href={sea.image} target="_blank"><b>Image</b></a></p>
+								<p className='text-center'>{sea.museumPhrase} </p>
+							</div>
+						)}  
+				</div>
+				<br/>
+				
+				<div ref={this.scrollDivSongs}>
+					<h3 className='text-center'><b>Songs:</b></h3>
+					<h5 className='text-center'>Number of songs: {(this.state.songsFiltered).length}</h5>
+						{(this.state.songsFiltered).map((song, i) =>
+							<div class="borderdiv" key={i}>
+								<h5 className='text-center'><b>{song.name}</b></h5>
+								<p className='text-center'><a href={song.image} target="_blank"><b>Image</b></a> &emsp; <a href={song.music} target="_blank"><b>Music</b></a></p>
+							</div>
+						)}  
+				</div>
+				<br/>
+				
+				<div ref={this.scrollDivVillagers}>
+					<h3 className='text-center'><b>Villagers:</b></h3>
+					<h5 className='text-center'>Number of villagers: {(this.state.villagersFiltered).length}</h5>
+						{(this.state.villagersFiltered).map((villager, i) =>
+							<div class="borderdiv" key={i}>
+								<h5 className='text-center'><b>{villager.name}</b></h5>
+								<p className='text-center'><a href={villager.image} target="_blank"><b>Image</b></a></p>
+							</div>
+						)}
+				</div>
+			</div>}
 		</div>
 		)
 	}
