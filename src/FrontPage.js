f2b2309e3f8b6e38bd05d2b24ffe1f36c45f965a
shengye000 <kevinyesheng@gmail.com>
2020-09-17 15:12:09 -0500

    Started on new topics. Changed front page and navigation bar.

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -48,7 +48,7 @@ class FrontPage extends React.Component{
 							sell you paintings or sculptures. Be careful though, as his artworks can be forgeries. </p>
 
 							<LinkContainer to="/art">
-								<Button variant="outline-primary mt-auto" size="lg"> Click Here To Learn More </Button>
+								<Button variant="outline-primary mt-auto" size="lg"> Learn More </Button>
 							</LinkContainer>
 
 						</div>
@@ -62,7 +62,7 @@ class FrontPage extends React.Component{
 							creatures for Blather's Museum, your personal collection, your Critterpedia, or even for Flick!</p>
 
 							<LinkContainer to="/bugs">
-								<Button variant="outline-secondary mt-auto" size="lg"> Click Here To Learn More </Button>
+								<Button variant="outline-secondary mt-auto" size="lg"> Learn More </Button>
 							</LinkContainer>
 
 						</div>
@@ -76,15 +76,11 @@ class FrontPage extends React.Component{
 								as gifts and they may even put on some of them!</p>
 
 							<LinkContainer to="/clothes">
-								<Button variant="outline-success mt-auto" size="lg"> Click Here To Learn More </Button>
+								<Button variant="outline-success mt-auto" size="lg"> Learn More </Button>
 							</LinkContainer>
 
 						</div>
 					</div>
-				</div>
-				<br/>
-
-				<div class="card-deck">
 					<div class="card border-danger bg-light">
 						<div class="card-body  d-flex flex-column">
 							<h5 class="card-title" className='text-center'><b>Construction</b></h5>
@@ -93,11 +89,15 @@ class FrontPage extends React.Component{
 							slopes to go up levels, and even the outer style of your house! </p>
 
 							<LinkContainer to="/construction">
-								<Button variant="outline-danger mt-auto" size="lg"> Click Here To Learn More </Button>
+								<Button variant="outline-danger mt-auto" size="lg"> Learn More </Button>
 							</LinkContainer>
 
 						</div>
 					</div>
+				</div>
+				<br/>
+
+				<div class="card-deck">
 					<div class="card border-warning bg-light">
 						<div class="card-body d-flex flex-column">
 							<h5 class="card-title" className='text-center'><b>Fish</b></h5>
@@ -106,11 +106,24 @@ class FrontPage extends React.Component{
 							for Blather's Museum, your personal collection, your Critterpedia, or even for C. J. </p>
 
 							<LinkContainer to="/fish">
-								<Button variant="outline-warning mt-auto" size="lg"> Click Here To Learn More </Button>
+								<Button variant="outline-warning mt-auto" size="lg"> Learn More </Button>
 							</LinkContainer>
 
 						</div>
 					</div>		
+					<div class="card border-info bg-light">
+						<div class="card-body d-flex flex-column">
+							<h5 class="card-title" className='text-center'> <b>Floors</b> </h5>
+							<p class="card-text" className = 'text-center'> If you are tired of the bland flooring in
+							your rooms, try spicing it up with unique flooring designs. From simple wooden floors to
+							complex flooring that could move, these are sure to make your home more pretty.</p>
+
+							<LinkContainer to="/floors">
+								<Button variant="outline-info mt-auto" size="lg"> Learn More </Button>
+							</LinkContainer>
+
+						</div>
+					</div>
 					<div class="card border-info bg-light">
 						<div class="card-body d-flex flex-column">
 							<h5 class="card-title" className='text-center'><b>Fossils</b></h5>
@@ -119,7 +132,21 @@ class FrontPage extends React.Component{
 							to evaluate and you can see the bones of pre-historic animals. </p>
 
 							<LinkContainer to="/fossils">
-								<Button variant="outline-info mt-auto" size="lg"> Click Here To Learn More </Button>
+								<Button variant="outline-info mt-auto" size="lg"> Learn More </Button>
+							</LinkContainer>
+
+						</div>
+					</div>
+					<div class="card border-dark bg-light">
+						<div class="card-body d-flex flex-column">
+							<h5 class="card-title" className='text-center'><b>Items</b></h5>
+							<p class="card-text" className = 'text-center'> The second longest table, up to hundreds of items
+								are used to decorate your island. From instruments to furniture to posters, these items will
+								be sure to make your island look extremely pretty. (Note: Items include the houseware, wall-mounted, and misc.
+								categories in your inventory.)</p>
+
+							<LinkContainer to="/items">
+								<Button variant="outline-dark mt-auto" size="lg"> Learn More </Button>
 							</LinkContainer>
 
 						</div>
@@ -130,32 +157,18 @@ class FrontPage extends React.Component{
 				<div class="card-deck">
 					<div class="card border-primary bg-light">
 						<div class="card-body d-flex flex-column">
-							<h5 class="card-title" className='text-center'><b>Items</b></h5>
-							<p class="card-text" className = 'text-center'> Probably the longest table, up to hundreds of items
-								are used to decorate your island. From instruments to furniture to posters, these items will
-								be sure to make your island look extremely pretty. (Note: Items include the houseware, wall-mounted, and misc.
-								categories in your inventory.)</p>
-
-							<LinkContainer to="/items">
-								<Button variant="outline-primary mt-auto" size="lg"> Click Here To Learn More </Button>
-							</LinkContainer>
-
-						</div>
-					</div>
-					<div class="card border-secondary bg-light">
-						<div class="card-body d-flex flex-column">
 							<h5 class="card-title" className='text-center'><b>Reactions</b></h5>
 							<p class="card-text" className = 'text-center'> From happy faces to heartbroken symbols, reactions
 							can be used by all villagers and players to show their feelings. Use these to communicate with your
 							friends and even take more accuracte photos with an appropriate reaction mixed in.</p>
 
 							<LinkContainer to="/reactions">
-								<Button variant="outline-secondary mt-auto" size="lg"> Click Here To Learn More </Button>
+								<Button variant="outline-primary mt-auto" size="lg"> Learn More </Button>
 							</LinkContainer>
 
 						</div>
 					</div>
-					<div class="card border-success bg-light">
+					<div class="card border-secondary bg-light">
 						<div class="card-body d-flex flex-column">
 							<h5 class="card-title" className='text-center'><b>Recipes</b></h5>
 							<p class="card-text" className = 'text-center'> A new feature in the series, crafting has allowed
@@ -163,7 +176,34 @@ class FrontPage extends React.Component{
 							that some DIY recipes can only be given by certain villagers with a specific personality?</p>
 
 							<LinkContainer to="/recipes">
-								<Button variant="outline-success mt-auto" size="lg"> Click Here To Learn More </Button>
+								<Button variant="outline-secondary mt-auto" size="lg"> Learn More </Button>
+							</LinkContainer>
+
+						</div>
+					</div>
+					<div class="card border-success bg-light">
+						<div class="card-body d-flex flex-column">
+							<h5 class="card-title" className='text-center'> <b>Rugs</b> </h5>
+							<p class="card-text" className = 'text-center'> Rugs are another way to decorate the interior
+							of your house. Place one anywhere on the floor for a better way to decorate your house. 
+							Think carefully though, as rugs cannot overlap with one another. </p>
+
+							<LinkContainer to="/rugs">
+								<Button variant="outline-success mt-auto" size="lg"> Learn More </Button>
+							</LinkContainer>
+
+						</div>
+					</div>
+					<div class="card border-danger bg-light">
+						<div class="card-body d-flex flex-column">
+							<h5 class="card-title" className='text-center'><b>Sea Creatures</b></h5>
+							<p class="card-text" className = 'text-center'> Added in the Wave One of the Summer Update of 2020, you can
+							go to Nook's Cranny and buy a swimsuit to swim in the ocean. A key element also included diving for
+							sea creatures previously inaccessible via a fishing rod. You have to be quick, as these animals
+							can swim away from you! </p>
+
+							<LinkContainer to="/sea">
+								<Button variant="outline-danger mt-auto" size="lg"> Learn More </Button>
 							</LinkContainer>
 
 						</div>
@@ -171,20 +211,6 @@ class FrontPage extends React.Component{
 				</div>
 				<br/>
 				<div class="card-deck">
-					<div class="card border-danger bg-light">
-						<div class="card-body d-flex flex-column">
-							<h5 class="card-title" className='text-center'><b>Sea Creatures</b></h5>
-							<p class="card-text" className = 'text-center'> Added in the Wave One of the Summer Update of 2020, you can
-							go to Nook's Cranny and buy a swimsuit to swim in the ocean. A key element also included diving for more
-							sea creatures previously inaccessible via a fishing rod. You may have to be quick, as these animals
-							can swim away from you if you are not fast enough to catch them! </p>
-
-							<LinkContainer to="/sea">
-								<Button variant="outline-danger mt-auto" size="lg"> Click Here To Learn More </Button>
-							</LinkContainer>
-
-						</div>
-					</div>
 					<div class="card border-warning bg-light">
 						<div class="card-body d-flex flex-column">
 							<h5 class="card-title" className='text-center'><b>Songs</b></h5>
@@ -193,7 +219,20 @@ class FrontPage extends React.Component{
 							By then, you can talk to the talented K. K. Slider and request for any song to listen to while the credits roll.</p>
 
 							<LinkContainer to="/songs">
-								<Button variant="outline-warning mt-auto" size="lg"> Click Here To Learn More </Button>
+								<Button variant="outline-warning mt-auto" size="lg"> Learn More </Button>
+							</LinkContainer>
+
+						</div>
+					</div>
+					<div class="card border-info bg-light">
+						<div class="card-body d-flex flex-column">
+							<h5 class="card-title" className='text-center'> <b>Tools</b> </h5>
+							<p class="card-text" className = 'text-center'> Tools are essential items in your inventory
+							crucial to the development of your personal island. From nets to catch bugs to optional wands
+							to change appearance, many tools are required to change your island or do daily tasks. </p>
+
+							<LinkContainer to="/tools">
+								<Button variant="outline-info mt-auto" size="lg"> Learn More </Button>
 							</LinkContainer>
 
 						</div>
@@ -206,14 +245,26 @@ class FrontPage extends React.Component{
 							and they may even give you a picture of themselves as a token of your bond.</p>
 
 							<LinkContainer to="/villagers">
-								<Button variant="outline-info mt-auto" size="lg"> Click Here To Learn More </Button>
+								<Button variant="outline-info mt-auto" size="lg"> Learn More </Button>
 							</LinkContainer>
 
 						</div>
 					</div>	
+					<div class="card border-dark bg-light">
+						<div class="card-body d-flex flex-column">
+							<h5 class="card-title" className='text-center'> <b>Wallpapers</b> </h5>
+							<p class="card-text" className = 'text-center'> Sold mostly from Saharah, wallpapers are just one 
+							of the few ways to decorate your house. Get rid of the boring white wall and put up more 
+							interesting designs to your liking. Some wallpaper may even move! </p>
+
+							<LinkContainer to="/wallpapers">
+								<Button variant="outline-dark mt-auto" size="lg"> Learn More </Button>
+							</LinkContainer>
+
+						</div>
+					</div>
 				</div>
 				<br/>
-				
 			</div>
 			)
 		}
@@ -244,7 +295,7 @@ class FrontPage extends React.Component{
 				<br/>
 				
 				<BrowserView>
-					<div className='frontpagepadding'>
+					<div className='realfrontpagepadding'>
 						{page()}
 					</div>
 				</BrowserView>
